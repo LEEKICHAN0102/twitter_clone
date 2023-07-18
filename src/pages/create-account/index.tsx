e851f2e449151ae88719b04bddcce95068469249
Kichan <rlcks01537@gmail.com>
2023-07-18 11:49:27 +0900

    Initialize

--- src/pages/create-account/index.tsx
+++ src/pages/create-account/index.tsx
@@ -1,7 +1,7 @@
-const Home = () => {
+const CreateAccount = () => {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24"></main>
   );
 };
 
-export default Home;
+export default CreateAccount;
